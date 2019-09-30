bfea104489bba4f9595d302a24f6dfe72b6ce071
健太郎 深水 <moyashitaro10@gmail.com>
2019-10-01 00:41:52 +0900

    vbo

---
+++ HSVKit/VertexArray.cpp
@@ -0,0 +1,63 @@
+// ----------------------------------------------------------------
+// From Game Programming in C++ by Sanjay Madhav
+// Copyright (C) 2017 Sanjay Madhav. All rights reserved.
+// 
+// Released under the BSD License
+// See LICENSE in root directory for full details.
+// ----------------------------------------------------------------
+
+#include "VertexArray.h"
+#include <GLFW/gg.h>
+#include <GLFW/glfw3.h>
+#include<vector>
+
+using namespace std;
+
+
+VertexArray::VertexArray()
+{
+	// Create vertex array
+	
+}
+
+void VertexArray::load(const std::vector<float> verts, unsigned int numVerts) {
+	glGenVertexArrays(1, &mVertexArray);
+	glBindVertexArray(mVertexArray);
+
+	// Create vertex buffer
+	glGenBuffers(1, &mVertexBuffer);
+	glBindBuffer(GL_ARRAY_BUFFER, mVertexBuffer);
+	glBufferData(GL_ARRAY_BUFFER, numVerts * 8 * sizeof(float), &verts[0], GL_STATIC_DRAW);
+
+
+
+
+	// Create index buffer
+
+
+	// Specify the vertex attributes
+	// (For now, assume one vertex format)
+	// Position is 3 floats
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), 0);
+	// Normal is 3 floats
+	glEnableVertexAttribArray(1);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float),
+		reinterpret_cast<void*>(sizeof(float) * 3));
+	// Texture coordinates is 2 floats
+	glEnableVertexAttribArray(2);
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float),
+		reinterpret_cast<void*>(sizeof(float) * 6));
+}
+
+VertexArray::~VertexArray()
+{
+	glDeleteBuffers(1, &mVertexBuffer);
+	glDeleteBuffers(1, &mIndexBuffer);
+	glDeleteVertexArrays(1, &mVertexArray);
+}
+
+void VertexArray::SetActive()
+{
+	glBindVertexArray(mVertexArray);
+}
