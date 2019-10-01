c7763071ccc7b59e28df01b5bd24b7a1d816931d
健太郎 深水 <moyashitaro10@gmail.com>
2019-10-01 11:50:37 +0900

    pusg

---
+++ HSVKit/VertexArray.h
@@ -0,0 +1,34 @@
+// ----------------------------------------------------------------
+// From Game Programming in C++ by Sanjay Madhav
+// Copyright (C) 2017 Sanjay Madhav. All rights reserved.
+// 
+// Released under the BSD License
+// See LICENSE in root directory for full details.
+// ----------------------------------------------------------------
+
+#pragma once
+#include<vector>
+class VertexArray
+{
+public:
+	VertexArray();
+	~VertexArray();
+
+	void SetActive();
+	void load(const std::vector<float> verts, unsigned int numVerts);
+	unsigned int GetNumIndices() const { return mNumIndices; }
+	unsigned int GetNumVerts() const { return mNumVerts; }
+private:
+	// How many vertices in the vertex buffer?
+	unsigned int mNumVerts;
+	// How many indices in the index buffer
+	unsigned int mNumIndices;
+	// OpenGL ID of the vertex buffer
+	unsigned int mVertexBuffer;
+	// OpenGL ID of the index buffer
+	unsigned int mIndexBuffer;
+	// OpenGL ID of the vertex array object
+	unsigned int mVertexArray;
+
+
+};
