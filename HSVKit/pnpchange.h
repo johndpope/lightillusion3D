5d030ca7e27341ef2659dcd41992ed6c9d8b0834
健太郎 深水 <moyashitaro10@gmail.com>
2019-10-17 11:21:51 +0900

    exp

--- HSVKit/pnpchange.h
+++ HSVKit/pnpchange.h
@@ -61,7 +61,7 @@ public:
 		//dst = glm::mat4(1.0f);
 		//dst = glm::translate(glm::mat4(1.0f), glm::vec3((float)tvec.at<double>(0,0), (float)tvec.at<double>(1,0), (float)tvec.at<double>(2,0)));
 		//dst = glm::translate(dst, glm::vec3(0.0f,0.0f,-1.0f));
-		cout << to_string(dst) << endl;
+		//cout << to_string(dst) << endl;
 		
 
 	}
